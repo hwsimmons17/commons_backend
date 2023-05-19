190ae15f6025314dab8e4fef19301a8795daaaed
Hunter Simmons <hunterwilliamsimmons@gmail.com>
2023-05-19 09:31:44 -0600

    Allow cors

--- src/router.rs
+++ src/router.rs
@@ -1,26 +1,28 @@
 use std::sync::Arc;
 
 use crate::{
-    app_state::AppState, repository::user::{UserRepository, DynUserRepo}, routes::users::create_user,
+    app_state::AppState,
+    repository::user::{DynUserRepo, UserRepository},
+    routes::users::create_user,
 };
-use axum::{
-    routing::{post},
-    Router,
-};
+use axum::{routing::post, Router};
+use reqwest::Method;
 use tokio::sync::Mutex;
+use tower_http::cors::{Any, CorsLayer};
 
-pub fn create_router<U>(
-    user_repo: U,
-) -> Router
+pub fn create_router<U>(user_repo: U) -> Router
 where
     U: UserRepository,
 {
     let user_repo = Arc::new(Mutex::new(user_repo)) as DynUserRepo;
-    let app_state = AppState {
-        user_repo,
-    };
+    let app_state = AppState { user_repo };
 
     Router::new()
         .route("/user", post(create_user))
+        .layer(
+            CorsLayer::new()
+                .allow_methods([Method::GET, Method::POST])
+                .allow_origin(Any),
+        )
         .with_state(app_state)
 }
